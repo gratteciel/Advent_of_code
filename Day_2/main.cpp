364ed46d1ed1bb6da2d8d9e66b141cd7482ee292
emilian mitu <emilian.mitu@edu.ece.fr>
2022-12-08 08:42:41 +0100

    feat: part 1

--- Day_2/main.cpp
+++ Day_2/main.cpp
@@ -8,6 +8,7 @@
  * X: Rock
  * Y : Paper
  * Z : Scissors
+ * wins = +6
  *
  * Points :
  * X/R : 1
@@ -17,13 +18,11 @@
 
 #include <iostream>
 #include <vector>
-#include <fstream>
 #include <algorithm>
-#include <map>
+
 
 using namespace std;
 
-void resMatch(const std::vector<char>& match, int *opponent, int *user);
 int main() {
     //score for opponent
     int opponent = 0;
@@ -34,13 +33,14 @@ int main() {
     std::vector<char> match;
 
     //opening the file
-    string filenamePath(R"(C:\Users\mitue\OneDrive\Documents\Perso\adventofCode\advent_of_code_project\Day_2\data.txt)");
+    string filenamePath(R"(C:\Users\mitue\OneDrive\Documents\ECE\advent_of_code\Day_2\data.txt)");
     FILE *fileData = ::fopen(filenamePath.c_str(),"r");
     char output;
 
     if(!fileData){
         std::cout << "issue for opening the file" << std::endl;
     }
+    //insert data into a vector from the file
     while(!::feof(fileData)){// 2 characters per line
             output = getc(fileData);
             if(output !='\n')
@@ -51,68 +51,66 @@ int main() {
     }
     int i = 0;
 
-    for (int x = 0; x < match.size()/2 ; ++x) {
-        cout << match[i+x+2] << endl;
+    //can do it in a different function
+    //calculate the score
+    for (int x = 0; x+i+1 < match.size() ; ++x) {
+        switch (match[x+i+2]) {
+            case 'X': //rock
+                user += 1;
+                switch (match[x+i]) {
+                    case 'A':
+                        user += 3;
+                        break;
+                    case 'B':
+                        user += 0;
+                        break;
+                    case 'C':
+                        user += 6;
+                        break;
+                    default:
+                        return 1;
+                }
+                break;
+            case 'Y': //paper
+                user += 2;
+                switch (match[x+i]) {
+                    case 'A':
+                        user += 6;
+                        break;
+                    case 'B':
+                        user += 3;
+                        break;
+                    case 'C':
+                        user += 0;
+                        break;
+                    default:
+                        return 1;
+                }
+                break;
+            case 'Z': //scissors
+                user += 3;
+                switch (match[x+i]) {
+                    case 'A':
+                        user += 0;
+                        break;
+                    case 'B':
+                        user += 6;
+                        break;
+                    case 'C':
+                        user += 3;
+                        break;
+                    default:
+                        return 1;
+                }
+                break;
 
-        if ('X' == match[x+i+2] && match[x+i] == 'A' ) {
-            user += 1+3;
-            opponent += 1+3;
-        }
-        if ('Y' == match[x+i+2] && match[x+i] == 'B' ) {
-            user += 2+3;
-            opponent += 2+3;
-        }
-        if ('Z' == match[x+i+2] && match[x+i] == 'C' ) {
-            user += 3+3;
-            opponent += 3+3;
-        }
-        if (match[x+i] == 'A' && match[x+i+2] == 'Y') {
-            user += 2 +6;
-        }
-        if (match[x+i] == 'C' && match[x+i+ 2] == 'Y') {
-            opponent += 3+6;
-        }
-        if (match[x+i] == 'A' && match[x+i + 2] == 'Z') {
-            opponent += 1+6;
-        }
-        if (match[x+i] == 'B' && match[x+i+ 2] == 'Z') {
-            user += 3+6;
-        }
-        if (match[x+i] == 'B' && match[x+i + 2] == 'X') {
-            opponent += 2+6;
-        }
-        if (match[x+i] == 'C' && match[x+i + 2] == 'X') {
-            opponent += 3+6;
         }
         i +=2;
 
     }
+    cout << "Final score for you: ";
     cout << user << endl;
-    cout << opponent << endl << endl;
+
     return 0;
 }
 
-
-
-void resMatch(const std::vector<char>& match, int *opponent, int *user){
-//Create a map with each value
-//pair for each type ?
-//value of map second : 1: opponent wins , -1 : user wins, 0 :draw
-   //create a map with result in fonction of the letter
-/*   std::map<std::pair<char,char>,int> map = {
-           {make_pair('x+i','R'),1},
-           {make_pair('B','Y'),2},
-           {make_pair('C','Z'),3}
-   };
-    std::map<std::pair<char,char>,int>::iterator it;
-    int x+i = 0;
-    int y = 0;
-    std::vector<std::pair<char,char>> temp;
-
-    it = map.find(map[pair)*/
-
-
-
-
-
-}
